ed41874d7378ede5750f3104924d3401c29e8616
Amrut Sagar <amrutsagar06@gmail.com>
2022-01-30 20:06:26 +0530

    react hangman

--- hangman/src/components/Popup.js
+++ hangman/src/components/Popup.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { checkWin } from '../helpers/helpers';
 
 const Popup = ({ correctLetters, wrongLetters, selectedWord, setPlayable }) => {
   let finalMessage = '';
   let finalMessageRevealWord = '';
   let playable =  true;
-    return (
+  
+  if( checkWin(correctLetters, wrongLetters,selectedWord) == 'win' ) {
+      finalMessage = 'Congratulations!! You Won!!';
+      playable = false;
+
+  }else if (checkWin(correctLetters,wrongLetters,selectedWord) == 'lose' ) {
+      finalMessage = 'Unfortunately you lost';
+      finalMessageRevealWord =`...the word was: ${selectedWord}`;
+      playable= false;
+  }
+  
+  
+  return (
   <div className="popup-container" >
   <div className="popup">
       <h2>{finalMessage}</h2>
