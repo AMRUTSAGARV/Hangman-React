62f0623c0117d16de6b78ca0148a1c5359b70af6
Amrut Sagar <amrutsagar06@gmail.com>
2022-01-30 16:43:46 +0530

    hangman react

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Figure from './components/Figure';
 
 function App() {
   return (
     <>
 <Header />
+<div className='game-container'>
+  <Figure />
+</div>
     </>
   );
 }
