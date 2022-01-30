805f2c2dc637b7979bb2b0ea3dca880a3e383903
Amrut Sagar <amrutsagar06@gmail.com>
2022-01-30 18:47:22 +0530

    react hangman

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,18 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
 
 function App() {
+  const [playable,setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters]= useState([]);
+  const [wrongLetters, setWrongLetters]= useState([]);
+
+
+useEffect(() => {
+  const handleKeydown = event => {
+    const { key, keyCode} = event;
+    
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+  
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [ ...currentLetters, letter]);
+  
+          } else {
+            // showNotification();
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(wrongLetters => [ ...wrongLetters, letter]);
+  
+          } else {
+            // showNotification();
+          }
+        }
+      }
+    
+  }
+  window.addEventListener('keydown', handleKeydown);
+
+  return () => window.removeEventListener('keydown', handleKeydown);
+
+}, [correctLetters, wrongLetters,playable]);   // adding array here it will only get call in the intial render.
+
+ 
   return (
     <>
 <Header />
 <div className='game-container'>
   <Figure />
+  <WrongLetters wrongLetters={wrongLetters}/>
+  <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
 </div>
     </>
   );
 }
 
-export default App;
+export default App; 
